d2d4721ceda495f68c40cbb849406d63d8894e3e
Danyal <malekiandan@gmail.com>
2025-06-25 14:00:41 +0330

    add a sample data migration, implements CommandLineRunner

--- src/main/java/dnl/tst/springlearning/cli/runner/BookAuthorMigrator.java
+++ src/main/java/dnl/tst/springlearning/cli/runner/BookAuthorMigrator.java
@@ -1,2 +1,74 @@
-package dnl.tst.springlearning.cli.runner;public class BookAuthorMigrator {
+package dnl.tst.springlearning.cli.runner;
+
+import dnl.tst.springlearning.model.Author;
+import dnl.tst.springlearning.model.Book;
+import dnl.tst.springlearning.model.Publisher;
+import dnl.tst.springlearning.repository.AuthorRepository;
+import dnl.tst.springlearning.repository.BookRepository;
+import dnl.tst.springlearning.repository.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class BookAuthorMigrator implements CommandLineRunner {
+    private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
+    private PublisherRepository publisherRepository;
+
+    public BookAuthorMigrator(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Book golestan = new Book();
+        golestan.setIsbn("g-12j");
+        golestan.setName("Golestan");
+
+        Book boostan = new Book();
+        boostan.setName("Boostan");
+        boostan.setIsbn("b03r");
+
+        Book shahname = new Book();
+        shahname.setName("Shahname");
+        shahname.setIsbn("sh34rj");
+
+        Author saadi = new Author();
+        saadi.setName("Saadi");
+        saadi.setAge(45);
+
+        Author ferdowsi = new Author();
+        ferdowsi.setName("Ferdowsi");
+        ferdowsi.setAge(65);
+
+        Publisher nashr_cheshme = new Publisher();
+        nashr_cheshme.setName("Nashr Cheshmeh");
+        nashr_cheshme.setAddress("Tehran, Revenue St");
+
+        List<Book> books = new ArrayList<>();
+        books.add(golestan);
+        books.add(boostan);
+        books.add(shahname);
+        Iterable<Book> savedBooks = bookRepository.saveAll(books);
+
+        nashr_cheshme.setBooks((List<Book>) savedBooks);
+        publisherRepository.save(nashr_cheshme);
+
+        List<Author> authors = new ArrayList<>();
+        authors.add(saadi);
+        authors.add(ferdowsi);
+        authorRepository.saveAll(authors);
+
+        golestan.getAuthors().add(saadi);
+        boostan.getAuthors().add(saadi);
+        shahname.getAuthors().add(ferdowsi);
+        bookRepository.saveAll(books);
+
+    }
 }
