e9c40521388cb3c74932b3677bbf896dfa329288
Danyal <malekiandan@gmail.com>
2025-06-25 16:53:30 +0330

    add book service

--- src/main/java/dnl/tst/springlearning/service/BookService.java
+++ src/main/java/dnl/tst/springlearning/service/BookService.java
@@ -1,2 +1,18 @@
-package dnl.tst.springlearning.service;public class BookService {
+package dnl.tst.springlearning.service;
+
+import dnl.tst.springlearning.model.Book;
+import dnl.tst.springlearning.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
 }
