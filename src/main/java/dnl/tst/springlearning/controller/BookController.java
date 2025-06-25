ecb44594f47e66f0981a7f6dda1ce7b622c4b095
Danyal <malekiandan@gmail.com>
2025-06-25 16:53:47 +0330

    add book controller

--- src/main/java/dnl/tst/springlearning/controller/BookController.java
+++ src/main/java/dnl/tst/springlearning/controller/BookController.java
@@ -1,2 +1,21 @@
-package dnl.tst.springlearning.controller;public class BookController {
+package dnl.tst.springlearning.controller;
+
+import dnl.tst.springlearning.service.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
 }
