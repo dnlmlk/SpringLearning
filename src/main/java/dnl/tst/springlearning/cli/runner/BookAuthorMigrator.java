0bf2b31ac8844b7af339bd89812dc251445f62de
Danyal <malekiandan@gmail.com>
2025-06-26 14:04:22 +0330

    add some books to data migration

--- src/main/java/dnl/tst/springlearning/cli/runner/BookAuthorMigrator.java
+++ src/main/java/dnl/tst/springlearning/cli/runner/BookAuthorMigrator.java
@@ -28,14 +28,32 @@ public class BookAuthorMigrator implements CommandLineRunner {
         Book golestan = new Book();
         golestan.setIsbn("g-12j");
         golestan.setName("Golestan");
+        golestan.setCategory("Poet");
 
         Book boostan = new Book();
         boostan.setName("Boostan");
         boostan.setIsbn("b03r");
+        boostan.setCategory("Poet");
 
         Book shahname = new Book();
         shahname.setName("Shahname");
         shahname.setIsbn("sh34rj");
+        shahname.setCategory("Poet");
+
+        Book fizik1 = new Book();
+        fizik1.setIsbn("f-12j");
+        fizik1.setName("Fizik 1");
+        fizik1.setCategory("Science");
+
+        Book riazi1 = new Book();
+        riazi1.setName("Riazi 1");
+        riazi1.setIsbn("r-b03r");
+        riazi1.setCategory("Science");
+
+        Book moadelat = new Book();
+        moadelat.setName("Moadelat");
+        moadelat.setIsbn("sf-sh34rj");
+        moadelat.setCategory("Math");
 
         Author saadi = new Author();
         saadi.setName("Saadi");
@@ -45,6 +63,10 @@ public class BookAuthorMigrator implements CommandLineRunner {
         ferdowsi.setName("Ferdowsi");
         ferdowsi.setAge(65);
 
+        Author iut = new Author();
+        iut.setName("Isfahan Uni Tech");
+        iut.setAge(89);
+
         Publisher nashr_cheshme = new Publisher();
         nashr_cheshme.setName("Nashr Cheshmeh");
         nashr_cheshme.setAddress("Tehran, Revenue St");
@@ -53,6 +75,9 @@ public class BookAuthorMigrator implements CommandLineRunner {
         books.add(golestan);
         books.add(boostan);
         books.add(shahname);
+        books.add(fizik1);
+        books.add(riazi1);
+        books.add(moadelat);
         Iterable<Book> savedBooks = bookRepository.saveAll(books);
 
         Set<Book> savedBooksSet = new HashSet<>((Collection) savedBooks);
@@ -62,11 +87,15 @@ public class BookAuthorMigrator implements CommandLineRunner {
         List<Author> authors = new ArrayList<>();
         authors.add(saadi);
         authors.add(ferdowsi);
+        authors.add(iut);
         authorRepository.saveAll(authors);
 
         golestan.getAuthors().add(saadi);
         boostan.getAuthors().add(saadi);
         shahname.getAuthors().add(ferdowsi);
+        riazi1.getAuthors().add(iut);
+        fizik1.getAuthors().add(iut);
+        moadelat.getAuthors().add(iut);
         bookRepository.saveAll(books);
 
     }
