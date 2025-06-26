eff48ffa13ddd1b7f6f4da2e8a5b7988b9aae72b
Danyal <malekiandan@gmail.com>
2025-06-26 14:01:42 +0330

    add two methods to book repostiory

--- src/main/java/dnl/tst/springlearning/repository/BookRepository.java
+++ src/main/java/dnl/tst/springlearning/repository/BookRepository.java
@@ -3,5 +3,9 @@ package dnl.tst.springlearning.repository;
 import dnl.tst.springlearning.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends CrudRepository<Book, Long> {
+    Iterable<Book> findBookByCategory(String category);
+    Book findBookByIdAndCategory(Long id, String category);
 }
